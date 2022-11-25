f42664b492f884261c437858ae720813bf6e4f4a
shohanjh09 <shohan.nsu.cse@gmail.com>
2022-11-25 18:21:56 +0600

    Add solution for Agoda with OPP implementation

---
+++ Agoda/Agoda.java
@@ -0,0 +1,190 @@
+import java.util.*;
+
+interface Category{
+    public List<Integer> getDices();
+}
+
+class ChanceCategory implements Category{
+    int[] dices;
+
+    public ChanceCategory(int[] dices){
+        this.dices = dices;
+    }
+
+    public List<Integer> getDices(){
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int dice: dices){
+            list.add(dice);
+        }
+
+        return list;
+    }
+}
+
+class NumberCategory implements Category{
+    int[] dices;
+    int catId;
+
+    public NumberCategory(int[] dices, int catId){
+        this.dices = dices;
+        this.catId = catId;
+    }
+
+    public List<Integer> getDices(){
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int dice: dices){
+            if(dice == this.catId){
+                list.add(dice);
+            }
+        }
+
+        return list;
+    }
+}
+
+class YatzyCategory implements Category{
+    int[] dices;
+    int UPPER_VALUE = 50;
+    int LOWER_VALUE = 0;
+
+    public YatzyCategory(int[] dices){
+        this.dices = dices;
+    }
+
+    public List<Integer> getDices(){
+        HashSet<Integer> set = new HashSet<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int dice: dices){
+            set.add(dice);
+        }
+
+        if(set.size() == 1){
+            list.add(this.UPPER_VALUE);
+        } else {
+            list.add(this.LOWER_VALUE);
+        }
+
+        return list;
+    }
+}
+
+class PairCategory implements Category{
+    int[] dices;
+
+    public PairCategory(int[] dices){
+        this.dices = dices;
+    }
+
+    public List<Integer> getDices(){
+        List<Integer> list = new ArrayList<Integer>();
+        TreeMap<Integer, Integer> pairDices = new TreeMap<Integer, Integer>();
+
+        for(int dice: dices){
+            pairDices.put(dice, pairDices.getOrDefault(dice, 0) + 1);
+        }
+
+        for(int key : pairDices.descendingKeySet()){
+            if(pairDices.get(key) / 2 > 0){
+                list.add(key * 2);
+                break;
+            }
+        }
+
+        return list;
+    }
+}
+
+class TwoPairCategory implements Category{
+    int[] dices;
+
+    public TwoPairCategory(int[] dices){
+        this.dices = dices;
+    }
+
+    public List<Integer> getDices(){
+        List<Integer> list = new ArrayList<Integer>();
+        TreeMap<Integer, Integer> pairDices = new TreeMap<Integer, Integer>();
+
+        for(int dice: dices){
+            pairDices.put(dice, pairDices.getOrDefault(dice, 0) + 1);
+        }
+
+        for(int key : pairDices.descendingKeySet()){
+            if(pairDices.get(key) / 2 > 0){
+                int divResult = pairDices.get(key) / 2;
+                list.add(key * 2 * divResult);
+            }
+        }
+
+        return list;
+    }
+}
+
+class Score {
+    Category category;
+
+    public Score(Category category){
+        this.category = category;
+    }
+
+    public int getSum(){
+        List<Integer> dices = this.category.getDices();
+        int sum = 0;
+
+        for(int dice: dices){
+            sum += dice;
+        }
+
+        return sum;
+    }
+}
+
+
+public class Agoda {
+    public static String[] NUMBER_CATEGORY_LIST = {"", "one", "two", "three", "four", "five", "six"};
+    public static String CHANCE = "chance";
+    public static String YATZY = "yatzy";
+    public static String PAIR = "pair";
+
+    public static String TWO_PAIR = "two-pair";
+
+    public static int getCategoryId(String categoryName){
+       for(int i = 0; i < NUMBER_CATEGORY_LIST.length; i++){
+           if(NUMBER_CATEGORY_LIST[i].equals(categoryName)){
+               return i;
+           }
+       }
+
+       return -1;
+    }
+
+    public static int getScore(int[] dices, String categoryName){
+        int catId = getCategoryId(categoryName);
+        Category category;
+
+        if(catId > 0){
+            category = new NumberCategory(dices, catId);
+        } else if(categoryName.equals(CHANCE)){
+            category = new ChanceCategory(dices);
+        } else if(categoryName.equals(YATZY)){
+            category = new YatzyCategory(dices);
+        } else if(categoryName.equals(PAIR)){
+            category = new PairCategory(dices);
+        } else{
+            category = new TwoPairCategory(dices);
+        }
+
+        Score score = new Score(category);
+
+        return score.getSum();
+    }
+
+
+    public static void main(String[] args) {
+        int sum = getScore(new int[]{1,1,1,2, 2}, "two");
+        System.out.println(sum);
+    }
+}
---
+++ Agoda/README.md
@@ -0,0 +1,43 @@
+Yatzy dice game
+In yatzy player is rolling 5 dices and then select the category, such as ones, twos, pair, two pairs etc (see the rules below).
+
+If the roll is compatible with the category, the player gets a score for the roll according to the rules. If the roll is not compatible with the category, the player scores zero for the roll.
+
+Your task is to score a GIVEN roll in a GIVEN category.
+
+Categories and Scoring Rules
+Chance:
+The player scores the sum of all dice, no matter what they read. For example:
+
+1,1,3,3,6 placed on “chance” scores 14 (1+1+3+3+6)
+4,5,5,6,1 placed on “chance” scores 21 (4+5+5+6+1)
+
+Yatzy:
+If all dice have the same number, the player scores 50 points. For example:
+
+1,1,1,1,1 placed on “yatzy” scores 50
+1,1,1,2,1 placed on “yatzy” scores 0
+
+Ones, Twos, Threes, Fours, Fives, Sixes:
+The player scores the sum of the dice that reads one, two, three, four, five or six, respectively. For example:
+
+1,1,2,4,4 placed on “fours” scores 8 (4+4)
+2,3,2,5,1 placed on “twos” scores 4 (2+2)
+3,3,3,4,5 placed on “ones” scores 0
+
+Pair:
+The player scores the sum of the two highest matching dice. For example, when placed on “pair”:
+
+3,3,3,4,4 scores 8 (4+4)
+1,1,6,2,6 scores 12 (6+6)
+3,3,3,4,1 scores 6 (3+3)
+3,3,3,3,1 scores 6 (3+3)
+
+Two pairs:
+If there are two pairs of dice with the same number, the player scores the sum of these dice. For example, when placed on “two pairs”:
+
+1,1,2,3,3 scores 8 (1+1+3+3)
+1,1,2,3,4 scores 0
+1,1,2,2,2 scores 6 (1+1+2+2)
+
+[execution time limit] 3 seconds (java)
